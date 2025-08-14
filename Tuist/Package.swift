9740a99ac9190004d76ffb0248eb777b94b0a024
DMelisena <demonlordx777@gmail.com>
2025-08-15 04:13:39 +0800

    ci: update firebase on package.swift

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version: 6.0
+// swift-tools-version: 6.0
 import PackageDescription
 
 #if TUIST
@@ -15,6 +16,10 @@ let package = Package(
         .package(
             url: "https://github.com/google-gemini/generative-ai-swift",
             .upToNextMajor(from: "0.5.6")
+        ),
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            .upToNextMajor(from: "11.0.0")
         ),
     ]
 )
